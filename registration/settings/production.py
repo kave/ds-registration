4562517bfa73bc9a42b6ee8649d56aa0eed0783b
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2015-10-05 15:17:34 -0400

    got rid of s3 serving

--- registration/settings/production.py
+++ registration/settings/production.py
@@ -6,11 +6,3 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {'default': dj_database_url.config()}
-
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-
-STATICFILES_STORAGE = 'core.storage.S3PipelineManifestStorage'
-STATIC_URL = 'http://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
-
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_FILE_OVERWRITE = True
