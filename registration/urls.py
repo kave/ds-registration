31e7c35640e3d631274e0db1d149dba682b8b9f8
etapau <etapau@gmail.com>
2015-10-04 21:50:06 -0400

    registration site

--- registration/urls.py
+++ registration/urls.py
@@ -1,15 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
-admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'registration.views.home', name='home'),
-    # url(r'^registration/', include('registration.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('core.urls', namespace='core')),
 )
